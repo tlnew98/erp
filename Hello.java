b79289e30f7052d9d90fbf5739277dae61960be9
tlnew <798331132@qq.com>
2017-07-09 19:55:54 +0800

    change Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public  class Hello{
 	public static void mian(String[]args){
 		System.out.println("Hello World");
 		System.out.println("单小楼");
+		System.out.println("单小楼");
+		System.out.println("单小楼");
 	}
 }
